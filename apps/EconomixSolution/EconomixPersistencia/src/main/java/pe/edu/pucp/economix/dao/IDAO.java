9561d391780db7e6a47c4c14163b9a6fc38a4118
Gael Arias <ariasgael@gmail.com>
2026-04-17 00:31:13 -0500

    Creation EconomixPersistencia - IDAO

---
+++ apps/EconomixSolution/EconomixPersistencia/src/main/java/pe/edu/pucp/economix/dao/IDAO.java
@@ -0,0 +1,12 @@
+package pe.edu.pucp.economix.dao;
+
+import java.util.List;
+
+public interface IDAO <T> {
+    int insertar(T objeto);
+    int modificar(T objeto);
+    int eliminar(int id);
+    T buscarPorId(int id);
+    List<T> listarTodas();
+}
+
