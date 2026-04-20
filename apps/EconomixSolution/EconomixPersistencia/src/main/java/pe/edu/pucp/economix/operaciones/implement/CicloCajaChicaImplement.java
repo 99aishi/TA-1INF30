d35fca9ab87d5a579f71e6e2a12a4b29b9174bfd
Gael Arias <ariasgael@gmail.com>
2026-04-20 14:41:31 -0500

    Renombrado de Implements CicloCaja-ComprobantePago

--- apps/EconomixSolution/EconomixPersistencia/src/main/java/pe/edu/pucp/economix/operaciones/implement/CicloCajaChicaImplement.java
+++ apps/EconomixSolution/EconomixPersistencia/src/main/java/pe/edu/pucp/economix/operaciones/implement/CicloCajaChicaImplement.java
@@ -1,12 +1,8 @@
 package pe.edu.pucp.economix.operaciones.implement;
 
-import pe.edu.pucp.economix.operaciones.dao.IComprobantePagoDAO;
-
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
-public class IComprobantePagoImplement {
+public class CicloCajaChicaImplement {
     private Connection con;
     private Statement st;
     private PreparedStatement pst;
--- apps/EconomixSolution/EconomixPersistencia/src/main/java/pe/edu/pucp/economix/operaciones/implement/ComprobantePagoImplement.java
+++ apps/EconomixSolution/EconomixPersistencia/src/main/java/pe/edu/pucp/economix/operaciones/implement/ComprobantePagoImplement.java
@@ -1,12 +1,8 @@
 package pe.edu.pucp.economix.operaciones.implement;
 
-import pe.edu.pucp.economix.operaciones.dao.ICicloCajaChicaDAO;
-
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
-public class ICicloCajaChicaImplement {
+public class ComprobantePagoImplement {
     private Connection con;
     private Statement st;
     private PreparedStatement pst;
