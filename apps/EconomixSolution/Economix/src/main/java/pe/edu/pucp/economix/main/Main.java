e7e74c49ca9dc194961bda51fc217e4792786b2a
Gael Arias <ariasgael@gmail.com>
2026-04-27 14:56:50 -0500

    Limpieza Main

--- apps/EconomixSolution/Economix/src/main/java/pe/edu/pucp/economix/main/Main.java
+++ apps/EconomixSolution/Economix/src/main/java/pe/edu/pucp/economix/main/Main.java
@@ -1,34 +1,26 @@
 package pe.edu.pucp.economix.main;
 
-import pe.edu.pucp.economix.operaciones.implement.RendicionImplement;
-import pe.edu.pucp.economix.operaciones.model.*;
+import java.util.List;
+
+import pe.edu.pucp.economix.operaciones.model.CicloCajaChica;
+import pe.edu.pucp.economix.operaciones.model.ComprobantePago;
+import pe.edu.pucp.economix.operaciones.model.SolicitudGasto;
 import pe.edu.pucp.economix.operaciones.test.CicloCajaChicaTest;
 import pe.edu.pucp.economix.operaciones.test.ComprobantePagoTest;
 import pe.edu.pucp.economix.operaciones.test.SolicitudGastoTest;
-import pe.edu.pucp.economix.rrhh.dao.IAreaDAO;
-import pe.edu.pucp.economix.rrhh.implement.*;
-import pe.edu.pucp.economix.rrhh.model.*;
+import pe.edu.pucp.economix.rrhh.model.Area;
+import pe.edu.pucp.economix.rrhh.model.Empleado;
+import pe.edu.pucp.economix.rrhh.model.Rol;
 import pe.edu.pucp.economix.rrhh.test.AdministradorTest;
 import pe.edu.pucp.economix.rrhh.test.AreaTest;
 import pe.edu.pucp.economix.rrhh.test.EmpleadoTest;
 import pe.edu.pucp.economix.rrhh.test.RolTest;
-import pe.edu.pucp.economix.tesoreria.dao.ICajaChicaDAO;
-import pe.edu.pucp.economix.tesoreria.dao.ICuentaBancariaDAO;
-import pe.edu.pucp.economix.tesoreria.dao.IEntregaARendirDAO;
-import pe.edu.pucp.economix.tesoreria.dao.IMonedaDAO;
-import pe.edu.pucp.economix.tesoreria.implement.CajaChicaImplement;
-import pe.edu.pucp.economix.tesoreria.implement.CuentaBancariaImplement;
-import pe.edu.pucp.economix.tesoreria.implement.EntregaARendirImplement;
-import pe.edu.pucp.economix.tesoreria.implement.MonedaImplement;
-import pe.edu.pucp.economix.tesoreria.model.*;
+import pe.edu.pucp.economix.tesoreria.model.CajaChica;
+import pe.edu.pucp.economix.tesoreria.model.Moneda;
 import pe.edu.pucp.economix.tesoreria.test.CajaChicaTest;
 import pe.edu.pucp.economix.tesoreria.test.CuentaBancariaTest;
 import pe.edu.pucp.economix.tesoreria.test.EntregaARendirTest;
 import pe.edu.pucp.economix.tesoreria.test.MonedaTest;
-
-import javax.naming.ldap.ControlFactory;
-import java.util.Date;
-import java.util.List;
 
 public class Main {
     public static void main(String []args){
@@ -57,239 +49,6 @@ public class Main {
         List<CicloCajaChica> ciclos = CicloCajaChicaTest.pruebaInsercion(listaCajasChicas.get(0), listaCajasChicas.get(1));
         List<SolicitudGasto> solicitudes = SolicitudGastoTest.pruebaInsercion(ciclos.get(0), empleados.get(1), empleados.get(2));
         List<ComprobantePago> facturas = ComprobantePagoTest.pruebaInsercion(solicitudes.getFirst(), monedas.getFirst());
-
-
-        // CHECK
-
-//        ICuentaBancariaDAO bDao=new CuentaBancariaImplement();
-//        CuentaBancaria cuenta= new CuentaBancaria();
-//        cuenta.setNombreBanco("BCP");
-//        cuenta.setNumeroBancario("444");
-//        cuenta.setCci("555");
-//        cuenta.setIdCuenta(2);
-//
-//        Moneda m=new Moneda();
-//        m.setIdMoneda(4);
-//        cuenta.setMoneda(m);
-//        Empleado e = new Empleado();
-//        e.setUsuarioID(2);
-//        cuenta.setAdministrador(e);
-
-        //TES CAJA CHICA
-//        Area areaTI = new Area("Tecnología (TI)", "Infraestructura y Desarrollo");
-//        areaTI.setIdArea(1);
-//        IAreaDAO areadao=new AreaImplement();
-//        areadao.insertar(areaTI);
-//        ICajaChicaDAO cajachicaDAO=new CajaChicaImplement();
-//        CajaChica caja= new CajaChica("Prueba3",1000, EstadoFondo.Activo,2000,areaTI);
-//        System.out.println(caja.getIdFondo());
-//        caja.setIdFondo(2);
-//        cajachicaDAO.insertar(caja); // CHEEECK :v
-//        caja.setMontoTecho(3500);
-//        caja.setSaldoActual(1500);
-//        cajachicaDAO.modificar(caja);
-
-//        CajaChica solicitado = cajachicaDAO.buscarPorId(2);
-//        System.out.println(solicitado);
-//        cajachicaDAO.eliminar(2);
-//        solicitado=cajachicaDAO.buscarPorId(2);
-//        System.out.println(solicitado);
-
-//        List<CajaChica> cajas = cajachicaDAO.listarTodas();
-//        if(cajas!=null){
-//            for(CajaChica c : cajas){
-//                System.out.println(c);
-//                System.out.println("\n");
-//            }
-//        }
-
-        //TES ENTREGA A RENDIR
-
-
-//        // Definición de los 3 usuarios empleados
-//        Empleado emp1 = new Empleado(
-//                1, "Fabricio", "Cordova", "Luna", "hash_seguro_123",
-//                EstadoUsuario.Activo, "fabricio.cordova@pucp.edu.pe", "987654321"
-//        );
-//
-//        Empleado emp2 = new Empleado(
-//                2, "Ana", "Martínez", "Salazar", "hash_seguro_456",
-//                EstadoUsuario.Activo, "ana.martinez@empresa.com", "912345678"
-//        );
-//
-//        Empleado emp3 = new Empleado(
-//                3, "Roberto", "García", "Pérez", "hash_seguro_789",
-//                EstadoUsuario.Activo, "roberto.garcia@empresa.com", "933445566"
-//        );
-//
-//        IEntregaARendirDAO entregaDao= new EntregaARendirImplement();
-//
-//        EntregaARendir viajeArg = new EntregaARendir(
-//                "Viaje Buenos Aires",
-//                0.0,
-//                EstadoFondo.Activo,
-//                "Gastos de movilidad y viáticos para estancia en Argentina", //
-//                1200.0,
-//                new Date(), // Fecha de hoy
-//                null,       // Aún no se abre
-//                null,       // Aún no se cierra
-//                EstadoEntregaARendir.Pendiente,
-//                emp1,       // Solicitante: Fabricio
-//                emp2        //
-//        );
-        //entregaDao.insertar(viajeArg);
-        //viajeArg.setEstado(EstadoEntregaARendir.Aceptado);
-        //viajeArg.setIdFondo(9);
-        //entregaDao.modificar(viajeArg);
-        //EntregaARendir consultada= entregaDao.buscarPorId(9);
-        //System.out.println(consultada);
-
-        /* RRHH Pruebas
-        AreaImplement areaDAO = new AreaImplement();
-        RolImplement rolDAO = new RolImplement();
-        EmpleadoImplement empleadoDAO = new EmpleadoImplement();
-        AdministradorImplement adminDAO = new AdministradorImplement();
-
-
-        // 1. Area y Rol
-        Area areaTI = new Area("Tecnología (TI)", "Infraestructura y Desarrollo");
-        areaTI.setIdArea(areaDAO.insertar(areaTI));
-        Area areaFinanzas = new Area("Finanzas", "Gestión contable y tesorería");
-        areaFinanzas.setIdArea(areaDAO.insertar(areaFinanzas));
-        Area areaGerenciaGeneral = new Area("Gerencia General", "Administración de la empresa y su rumbo");
-        areaGerenciaGeneral.setIdArea(areaDAO.insertar(areaGerenciaGeneral));
-
-        List<Area> listaAreas = areaDAO.listarTodas();
-        for (Area a : listaAreas) {
-            System.out.println(a);
-        }
-        System.out.println();
-
-        Rol rolGerenteGeneral = new Rol("Gerente General", "Responsable de la empresa");
-        rolGerenteGeneral.setRolID(rolDAO.insertar(rolGerenteGeneral));
-        Rol rolGerente = new Rol("Gerente de Área", "Responsable de departamento");
-        rolGerente.setRolID(rolDAO.insertar(rolGerente));
-        Rol rolAnalista = new Rol("Analista Senior", "Especialista con experiencia");
-        rolAnalista.setRolID(rolDAO.insertar(rolAnalista));
-
-        List<Rol> listaRoles = rolDAO.listarTodas();
-        for (Rol r : listaRoles) {
-            System.out.println(r);
-        }
-        System.out.println();
-
-        // 2. Jefe
-        Empleado jefe = new Empleado();
-        jefe.setNombre("Pedro");
-        jefe.setApellidoPaterno("Páramo");
-        jefe.setEstado(EstadoUsuario.Activo);
-        jefe.setCorreoInstitucional("p.paramo@acervo.com");
-        jefe.setPassword("TILIN");
-        jefe.setArea(areaGerenciaGeneral);
-        jefe.setRol(rolGerenteGeneral);
-        jefe.setJefeDirecto(null);
-        jefe.setUsuarioID(empleadoDAO.insertar(jefe));
-
-        // Empleado 1: Augusto Pérez (TI)
-        Empleado emp1 = new Empleado();
-        emp1.setNombre("Augusto");
-        emp1.setApellidoPaterno("Pérez");
-        emp1.setApellidoMaterno("Niebla");
-        emp1.setEstado(EstadoUsuario.Activo);
-        emp1.setCorreoInstitucional("a.perez@acervo.com");
-        emp1.setPassword("TILIN");
-        emp1.setNumeroCelular("993000003");
-        emp1.setArea(areaTI);
-        emp1.setRol(rolAnalista);
-        emp1.setJefeDirecto(jefe);
-        emp1.setUsuarioID(empleadoDAO.insertar(emp1));
-
-        // Empleado 2: Esteban Trueba (Gerente de Finanzas)
-        Empleado emp2 = new Empleado();
-        emp2.setNombre("Esteban");
-        emp2.setApellidoPaterno("Trueba");
-        emp2.setApellidoMaterno("Del Valle");
-        emp2.setEstado(EstadoUsuario.Activo);
-        emp2.setCorreoInstitucional("e.trueba@acervo.com");
-        emp2.setPassword("TILIN");
-        emp2.setNumeroCelular("999000222");
-        emp2.setArea(areaFinanzas);
-        emp2.setRol(rolGerente);
-        emp2.setJefeDirecto(jefe);
-        emp2.setUsuarioID(empleadoDAO.insertar(emp2));
-
-        // Empleado 3: Santiago Nasar (Analista en Finanzas bajo Esteban)
-        Empleado emp3 = new Empleado();
-        emp3.setNombre("Santiago");
-        emp3.setApellidoPaterno("Nasar");
-        emp3.setApellidoMaterno("Linares");
-        emp3.setEstado(EstadoUsuario.Activo);
-        emp3.setCorreoInstitucional("s.nasar@acervo.com");
-        emp3.setPassword("TILIN");
-        emp3.setNumeroCelular("991000001");
-        emp3.setArea(areaFinanzas);
-        emp3.setRol(rolAnalista);
-        emp3.setJefeDirecto(emp2); // Su jefe es Esteban
-        emp3.setUsuarioID(empleadoDAO.insertar(emp3));
-
-
-        List<Empleado> listaEmpleados = empleadoDAO.listarTodas();
-        for (Empleado e : listaEmpleados) {
-            // Obtenemos los nombres de los objetos relacionados (Agregación)
-            String nombreArea = (e.getArea() != null) ? e.getArea().getNombre() : "N/A";
-            String tituloRol = (e.getRol() != null) ? e.getRol().getTitulo() : "N/A";
-
-            System.out.println(String.format("ID: %d | Empleado: %-25s | Correo: %-25s | Área: %-15s | Rol: %s",
-                    e.getUsuarioID(),
-                    e.getNombre() + " " + e.getApellidoPaterno(),
-                    e.getCorreoInstitucional(),
-                    nombreArea,
-                    tituloRol));
-        }
-        System.out.println();
-        // 4. Administradores
-        // Administrador 1: Soporte Técnico Principal
-        Administrador admin1 = new Administrador();
-        admin1.setNombre("Soporte");
-        admin1.setApellidoPaterno("Técnico");
-        admin1.setCorreoSoporte("soporte@empresa.com");
-        admin1.setPassword("TILIN");
-        admin1.setEstado(EstadoUsuario.Activo);
-        admin1.setUsuarioID(adminDAO.insertar(admin1));
-
-        List<Administrador> listaAdmins = adminDAO.listarTodas();
-        for (Administrador ad : listaAdmins) {
-            // Obtenemos los nombres de los objetos relacionados (Agregación)
-
-            System.out.println(String.format("ID: %d | Empleado: %-25s | Correo: %-25s",
-                    ad.getUsuarioID(),
-                    ad.getNombre() + " " + ad.getApellidoPaterno(),
-                    ad.getCorreoSoporte()));
-        }
-        */
-
-//        /* Operaciones Pruebas*/
-//        RendicionImplement rendicionDAO = new RendicionImplement();
-//
-//        Rendicion r1 = new Rendicion();
-//        r1.setFechaPresentacion(new Date());
-//        r1.setTotalDeclarado(1500.00);
-//        r1.setEstado(EstadoRendicion.EnEspera);
-//        r1.setComentario("Rendición de caja chica Finanzas - Semana 1");
-//        r1.setIdRendicion(rendicionDAO.insertar(r1));
-//
-//        Rendicion r2 = new Rendicion();
-//        r2.setFechaPresentacion(new Date());
-//        r2.setTotalDeclarado(850.00);
-//        r2.setEstado(EstadoRendicion.EnEspera);
-//        r2.setComentario("Viáticos viaje comercial Norte");
-//        r2.setIdRendicion(rendicionDAO.insertar(r2));
-//
-//        List<Rendicion> rendiciones = rendicionDAO.listarTodas();
-//        for (Rendicion r : rendiciones) {
-//            System.out.printf("ID: %d | Estado: %-10s | Monto: %-8s | Comentario: %s%n",
-//                    r.getIdRendicion(), r.getEstado(), r.getTotalDeclarado(), r.getComentario());
-//        }
 
     }
 
