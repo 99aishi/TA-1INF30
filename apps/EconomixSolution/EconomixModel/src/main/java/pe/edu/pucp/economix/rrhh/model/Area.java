bebe79624154c52063b8f958d27327a28e10c193
Gael Arias <ariasgael@gmail.com>
2026-05-01 02:44:44 -0500

    add: cajaChica, cuentasBancarias - update: toString

--- apps/EconomixSolution/EconomixModel/src/main/java/pe/edu/pucp/economix/rrhh/model/Area.java
+++ apps/EconomixSolution/EconomixModel/src/main/java/pe/edu/pucp/economix/rrhh/model/Area.java
@@ -1,28 +1,33 @@
 package pe.edu.pucp.economix.rrhh.model;
 
 import pe.edu.pucp.economix.operaciones.model.SolicitudGasto;
+import pe.edu.pucp.economix.tesoreria.model.CajaChica;
+import pe.edu.pucp.economix.tesoreria.model.CuentaBancaria;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Area{
-    private static int correlativoID = 1;
     private int idArea;
     private String nombre;
     private String descripcion;
+    //Relaciones
     private Empleado jefe;
-    private List<Empleado> empleados;
+    private CajaChica cajaChica;
+    private List<CuentaBancaria> cuentasBancarias;
 
     //Constructores
     public Area(){
-
     }
-    public Area(String nombre, String descripcion) {
 
-        this.idArea = this.correlativoID++;
+    public Area(int idArea, String nombre, String descripcion, Empleado jefe, CajaChica cajaChica, List<CuentaBancaria> cuentasBancarias) {
+        this.idArea = idArea;
         this.nombre = nombre;
         this.descripcion = descripcion;
-        this.empleados=new ArrayList<>();
+        this.jefe = jefe;
+        this.cajaChica = cajaChica;
+        this.cuentasBancarias = cuentasBancarias;
     }
 
     //Selectores
@@ -50,14 +55,32 @@ public class Area{
     public void setJefe(Empleado jefe) {
         this.jefe = jefe;
     }
+    public CajaChica getCajaChica() {
+        return cajaChica;
+    }
+    public void setCajaChica(CajaChica cajaChica) {
+        this.cajaChica = cajaChica;
+    }
+    public List<CuentaBancaria> getCuentasBancarias() {
+        return cuentasBancarias;
+    }
+    public void setCuentasBancarias(List<CuentaBancaria> cuentasBancarias) {
+        this.cuentasBancarias = cuentasBancarias;
+    }
 
     //Metodos
     @Override
-    public String toString(){
-        String cadena = "";
-        cadena += String.format("AREA: %d - %s - %s", this.getIdArea(), this.getNombre(), this.getDescripcion());
-        return cadena;
+    public String toString() {
+        return "Area{" +
+                "idArea=" + idArea +
+                ", nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", jefe=" + jefe +
+                ", cajaChica=" + cajaChica +
+                ", cuentasBancarias=" + cuentasBancarias +
+                '}';
     }
+
     public List<SolicitudGasto> obtenerGastosConsolidados() {
         // TODO: Retornar lista de gastos de todos los empleados del área (RF_22)
         return new java.util.ArrayList<>();
