fc0d1caffadf6699237d8b5be6c17deed25f6cf6
Gael Arias <ariasgael@gmail.com>
2026-05-02 16:06:03 -0500

    remove: join con area,empleado,usuario

--- database/Tesoreria/Procedimientos/cuenta_bancaria.sql
+++ database/Tesoreria/Procedimientos/cuenta_bancaria.sql
@@ -2,13 +2,13 @@ DELIMITER $$
 
 DROP PROCEDURE IF EXISTS pa_insertar_cuenta_bancaria $$
 CREATE PROCEDURE pa_insertar_cuenta_bancaria(
-	OUT  _id_cuenta_bancaria INT,
-	IN _nombre_banco VARCHAR(50),
-    IN _numero_cuenta VARCHAR(30),
-    IN _cci CHAR(20),
-    IN _id_moneda INT,
-    IN _id_usuario_titular INT, 
-    IN _id_area_titular INT
+	OUT p_id_cuenta_bancaria INT,
+	IN p_nombre_banco VARCHAR(50),
+    IN p_numero_cuenta VARCHAR(30),
+    IN p_cci CHAR(20),
+    IN p_id_moneda INT,
+    IN p_id_usuario_titular INT, 
+    IN p_id_area_titular INT
 )
 BEGIN
 
@@ -17,93 +17,88 @@ BEGIN
         id_moneda,id_usuario_titular, 
         id_area) 
 	VALUES 
-        (_nombre_banco,_numero_cuenta,_cci,
-        _id_moneda,_id_usuario_titular,
-        _id_area_titular);
+        (p_nombre_banco,p_numero_cuenta,p_cci,
+        p_id_moneda,p_id_usuario_titular,
+        p_id_area_titular);
 	
-	SET _id_cuenta_bancaria=@@last_insert_id;
+	SET p_id_cuenta_bancaria=@@last_insert_id;
 
 END$$
 
 DROP PROCEDURE IF EXISTS pa_modificar_cuenta_bancaria $$
 CREATE PROCEDURE pa_modificar_cuenta_bancaria(
-    IN _id_cuenta_bancaria INT,
-    IN _nombre_banco VARCHAR(50),
-    IN _numero_cuenta VARCHAR(30),
-    IN _cci CHAR(20),
-    IN _id_moneda INT,
-    IN _id_usuario_titular INT, 
-    IN _id_area_administradora INT
+    IN p_id_cuenta_bancaria INT,
+    IN p_nombre_banco VARCHAR(50),
+    IN p_numero_cuenta VARCHAR(30),
+    IN p_cci CHAR(20),
+    IN p_id_moneda INT,
+    IN p_id_usuario_titular INT, 
+    IN p_id_area_administradora INT
 )
 BEGIN
+    IF p_id_cuenta_bancaria IS NULL THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ID de la cuenta no válido';
+    END IF;
+
     UPDATE tes_cuenta_bancaria
-    SET nombre_banco = _nombre_banco,
-        numero_cuenta = _numero_cuenta,
-        cci = _cci,
-        id_moneda = _id_moneda,
-        id_usuario_titular = _id_usuario_titular,
-        id_area = _id_area_administradora
-    WHERE id_cuenta_bancaria = _id_cuenta_bancaria;
+    SET nombre_banco = p_nombre_banco,
+        numero_cuenta = p_numero_cuenta,
+        cci = p_cci,
+        id_moneda = p_id_moneda,
+        id_usuario_titular = p_id_usuario_titular,
+        id_area = p_id_area_administradora
+    WHERE id_cuenta_bancaria = p_id_cuenta_bancaria;
 END$$
 
 DROP PROCEDURE IF EXISTS pa_eliminar_cuenta_bancaria $$
 CREATE PROCEDURE pa_eliminar_cuenta_bancaria( 
-	IN _id_cuenta_bancaria INT
+	IN p_id_cuenta_bancaria INT
 )
 BEGIN 
-	UPDATE tes_cuenta_bancaria SET activa = 0 WHERE id_cuenta_bancaria = _id_cuenta_bancaria;
+    IF p_id_cuenta_bancaria IS NULL THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ID de la cuenta no válido';
+    END IF;
+	UPDATE tes_cuenta_bancaria SET activa = 0 WHERE id_cuenta_bancaria = p_id_cuenta_bancaria;
 
 END$$
 
 DROP PROCEDURE IF EXISTS pa_busqueda_por_id_cuenta_bancaria $$
 CREATE PROCEDURE pa_busqueda_por_id_cuenta_bancaria(
-    IN _id_cuenta_bancaria INT
+    IN p_id_cuenta_bancaria INT
 )
 BEGIN
+    IF p_id_cuenta_bancaria IS NULL THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ID de la cuenta no válido';
+    END IF;
+
     SELECT 
-        cb.id_cuenta_bancaria AS id,
-        cb.numero_cuenta,
-        cb.nombre_banco,
-        cb.cci,
-        m.id_moneda,
-        m.codigo_iso,
-        u.id_usuario,
-        u.nombres,
-        u.apellido_paterno,
-        u.apellido_materno, 
-        a.id_area
-    FROM tes_cuenta_bancaria cb
-    INNER JOIN tes_moneda m
-        ON cb.id_moneda = m.id_moneda
-    INNER JOIN rrhh_usuario u
-        ON cb.id_usuario_titular = u.id_usuario
-    LEFT JOIN rrhh_area a 
-        ON cb.id_area = a.id_area 
-    WHERE cb.id_cuenta_bancaria = _id_cuenta_bancaria ;
+        id_cuenta_bancaria,
+        numero_cuenta,
+        nombre_banco,
+        cci,
+        id_moneda,
+        id_usuario,
+        id_area
+    FROM tes_cuenta_bancaria 
+    WHERE id_cuenta_bancaria = p_id_cuenta_bancaria and activa = 1;
 END$$
 
 DROP PROCEDURE IF EXISTS pa_listar_cuentas_bancarias $$
 CREATE PROCEDURE pa_listar_cuentas_bancarias()
 BEGIN
+    IF p_id_cuenta_bancaria IS NULL THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ID de la cuenta no válido';
+    END IF;
     SELECT 
-        cb.id_cuenta_bancaria AS id,
-        cb.numero_cuenta,
-        cb.nombre_banco,
-        cb.cci,
-        m.id_moneda,
-        m.codigo_iso,
-        u.id_usuario, 
-        u.nombres,
-        u.apellido_paterno,
-        u.apellido_materno, 
-        a.id_area
+        id_cuenta_bancaria,
+        numero_cuenta,
+        nombre_banco,
+        cci,
+        id_moneda,
+        id_usuario,
+        id_area
     FROM tes_cuenta_bancaria cb
-    INNER JOIN tes_moneda m
-        ON cb.id_moneda = m.id_moneda
-    LEFT JOIN rrhh_usuario u
-        ON cb.id_usuario_titular = u.id_usuario
-    LEFT JOIN rrhh_area a 
-        ON cb.id_area = a.id_area;
+    WHERE activa = 1;
 END$$
 
 DELIMITER ;
