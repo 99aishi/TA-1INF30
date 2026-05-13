ad42839e3d2b99343f726860abec72f93fbf1cd6
Gael Arias <ariasgael@gmail.com>
2026-05-13 01:15:04 -0500

    add: Triggers Auditoria, Log General

---
+++ database/General/Auditoria.sql
@@ -0,0 +1,50 @@
+CREATE TABLE IF NOT EXISTS log_auditoria (
+    id_auditoria INT NOT NULL AUTO_INCREMENT,
+    nombre_tabla VARCHAR(100) NOT NULL,
+    tipo_evento VARCHAR(10) NOT NULL, -- Valores: 'INSERT', 'UPDATE', 'DELETE'
+    id_registro_afectado VARCHAR(50) NOT NULL,
+    valores_antiguos JSON, -- Estado previo
+    valores_nuevos JSON,   -- Estado final
+    
+    creado_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id_usuario_auditoria INT NULL,
+    
+    CONSTRAINT pk_log_auditoria PRIMARY KEY (id_auditoria)
+) ENGINE=InnoDB;
+
+CREATE INDEX idx_log_auditoria_tabla ON log_auditoria(nombre_tabla);
+CREATE INDEX idx_log_auditoria_evento ON log_auditoria(tipo_evento);
+CREATE INDEX idx_log_auditoria_creado ON log_auditoria(creado_at);
+CREATE INDEX idx_log_auditoria_usuario ON log_auditoria(id_usuario_auditoria);
+
+DELIMITER //
+
+
+DROP PROCEDURE IF EXISTS pa_insertar_auditoria//
+CREATE PROCEDURE pa_insertar_auditoria (
+    IN p_nombre_tabla VARCHAR(100),
+    IN p_tipo_evento VARCHAR(10),
+    IN p_id_registro VARCHAR(50),
+    IN p_valores_antiguos JSON,
+    IN p_valores_nuevos JSON,
+    IN p_id_usuario INT
+)
+BEGIN
+    INSERT INTO log_auditoria (
+        nombre_tabla, 
+        tipo_evento, 
+        id_registro_afectado, 
+        valores_antiguos, 
+        valores_nuevos, 
+        id_usuario_auditoria
+    ) VALUES (
+        p_nombre_tabla,
+        p_tipo_evento,
+        p_id_registro,
+        p_valores_antiguos,
+        p_valores_nuevos,
+        p_id_usuario
+    );
+END //
+
+DELIMITER ;
