1781a4fa95d903134b40382d5889f8e9ae5206a7
marialegi <maria.nalb031005@gmail.com>
2026-06-02 23:29:26 -0500

    Update IMonedaDAO.java

--- apps/EconomixSolution/EconomixPersistencia/src/main/java/pe/edu/pucp/economix/tesoreria/idao/IMonedaDAO.java
+++ apps/EconomixSolution/EconomixPersistencia/src/main/java/pe/edu/pucp/economix/tesoreria/idao/IMonedaDAO.java
@@ -4,4 +4,6 @@ import pe.edu.pucp.economix.idao.IDAO;
 import pe.edu.pucp.economix.tesoreria.model.Moneda;
 
 public interface IMonedaDAO extends IDAO <Moneda> {
+  List<Moneda> listarMonedas_X_codigoISO_nombre_simbolo(String busqueda) throws SQLException ;
+  List<Moneda> listarMonedas_X_estado(boolean activa) throws SQLException;
 }
