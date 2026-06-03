dc5c7c08b3484a6c84d6dd30fdc48246bfd647e6
Gael Arias <ariasgael@gmail.com>
2026-06-03 16:11:55 -0500

    Add: Servicios Configuration-REST

--- apps/EconomixSolution/EconomixNegocio/src/main/java/pe/edu/pucp/economix/tesoreria/ibo/IMonedaBO.java
+++ apps/EconomixSolution/EconomixNegocio/src/main/java/pe/edu/pucp/economix/tesoreria/ibo/IMonedaBO.java
@@ -3,6 +3,8 @@ package pe.edu.pucp.economix.tesoreria.ibo;
 import pe.edu.pucp.economix.ibo.IBaseBO;
 import pe.edu.pucp.economix.tesoreria.model.Moneda;
 
+import java.util.List;
+
 public interface IMonedaBO extends IBaseBO<Moneda> {
     List<Moneda> listarMonedas_X_codigoISO_nombre_simbolo(String busqueda) throws Exception;
     List<Moneda> listarMonedas_X_estado(boolean activa) throws Exception;
